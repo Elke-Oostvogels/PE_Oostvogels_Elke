39efc32bfad84a2ab88eb850b86a71f08d360c32
Elke Oostvogels <r0830699@student.thomasmore.be>
2021-03-06 17:17:10 +0100

    bouwen structuur functies

---
+++ js/ScriptFormulier.js
@@ -0,0 +1,30 @@
+let errors = [];
+
+function validateForm() {
+    checkEmptyField();
+}
+
+// Functcie controleerd velden ofdeze ingevuld zijn
+function checkEmptyField(veld, melding) {
+
+}
+
+// Functie controle op een geldig email 
+function validateEmail(emailadres) {
+
+}
+
+// Functie controle wachtwoord en herhaald wachtwoord
+function validateWachtwoord() {
+
+}
+
+// Functie controle betalingswijze
+function validatePayment(veld) {}
+
+// Functie controle postcode
+function checkPC() {
+
+}
+
+// Functie
