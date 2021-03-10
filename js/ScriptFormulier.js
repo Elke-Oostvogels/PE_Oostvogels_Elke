ab8a54d0772a7dc9980419120361f3ba849a0f3b
Elke Oostvogels <r0830699@student.thomasmore.be>
2021-03-10 17:27:14 +0100

    functie betaling

--- js/ScriptFormulier.js
+++ js/ScriptFormulier.js
@@ -1,4 +1,8 @@
 let errors = [];
+let melding = [];
+let meldingBetaling = "";
+let veld = document.forms["registratie"]["exampleInputNaam"];
+veld += document.forms["registratie"]["exampleInputVoornaam"];
 
 
 
@@ -7,17 +11,30 @@ function validateForm() {
     validateWachtwoord();
     let postcode = document.forms["registratie"]["exampleInputPostcode"].value;
     checkPC(postcode);
-
+    document.forms["registratie"]["exampleFormControlEmail"].value;
+    // let mail = validateEmail(emailadres);
+    errors = checkEmptyField(veld, melding);
+    let betaling = document.getElementById("exampleBetaling").value;
+    meldingBetaling = validatePayment(betaling);
+    console.log(meldingBetaling);
 }
 
 // Functcie controleerd velden ofdeze ingevuld zijn
 function checkEmptyField(veld, melding) {
-
+    // errors verplichte velden
+    // errors += "Het veld voornaam is vereist";
+    // errors += "Het veld naam is vereist";
+    // errors += "Het veld gebruikersnaam is vereist";
+    // errors += "Het veld adres is vereist";
+    // errors += "Het veld land is vereist";
+    // errors += "Het veld provincie is vereist";
 }
 
 // Functie controle op een geldig email 
 function validateEmail(emailadres) {
-
+    if (emailadres.validity.typeMismatch) {} else {
+        errors += "E-mail adres is niet correct";
+    }
 }
 
 // Functie controle wachtwoord en herhaald wachtwoord
@@ -32,25 +49,24 @@ function validateWachtwoord() {
         errors += "Het wachtwoord moet uit 7 of meer karakters bestaan";
     } else if (wachtwoord1 !== wachtwoord2) {
         errors += "Je wachtwoorden komen niet overeen";
-    } else {
-        return false;
     }
     console.log(errors, "melding");
 }
 
 // Functie controle betalingswijze
 function validatePayment(veld) {
-
+    if (veld != "") {
+        return "Je betalingswijze is " + veld;
+    }
 }
 
 // Functie controle postcode
 function checkPC(veld) {
     if (veld == "") {
         errors += "Het veld postcode is vereist";
-    } else if (veld < 1000 || veld > 9999) {
+    } // 
+    else if (veld < 1000 || veld > 9999) {
         errors += "De waarde van postcode moet tussen de 1000 en 9999 liggen.";
-    } else {
-        return false;
     }
     console.log(errors, "melding");
 }
@@ -60,18 +76,12 @@ function checkPC(veld) {
 
 // Gebruikte websites
 /* Controle van lege velden
-https://www.w3schools.com/howto/howto_js_validation_empty_input.asp*/
+https://www.w3schools.com/howto/howto_js_validation_empty_input.asp
+Controle email-adress
+https://developer.mozilla.org/en-US/docs/Learn/Forms/Form_validation
+Oproepen waarde select option
+https://www.w3schools.com/jsref/prop_select_value.asp*/
 
-// errors verplichte velden
-// errors += "Het veld voornaam is vereist";
-// errors += "Het veld naam is vereist";
-// errors += "Het veld gebruikersnaam is vereist";
-// errors += "Het veld adres is vereist";
-// errors += "Het veld land is vereist";
-// errors += "Het veld provincie is vereist";
-
-// error email
-// errors += "E-mail adres is niet correct";
 
 // errors voorwaarde
 // errors += "Je moet de algemene voorwaarden accepteren.";
