e982746bb10146d4cc8730443011a63c9b03d1e2
Elke Oostvogels <r0830699@student.thomasmore.be>
2021-03-16 15:00:33 +0100

    functie controle velden

--- js/ScriptFormulier.js
+++ js/ScriptFormulier.js
@@ -14,20 +14,35 @@ function validateForm() {
     let betaling = document.getElementById("exampleBetaling").value;
     let postcode = document.forms["registratie"]["exampleInputPostcode"].value;
     let email = document.forms["registratie"]["exampleFormControlEmail"].value;
-    // let veld = document.forms["registratie"]["InputNaam"].value;
-    // veld += document.forms["registratie"]["exampleInputVoornaam"].value;
+
+    function Gegevens(voornaam, achternaam, gebruikersnaam, adres, land, provincie) {
+        this.voornaam = voornaam;
+        this.achternaam = achternaam;
+        this.gebruikersnaam = gebruikersnaam;
+        this.adres = adres;
+        this.land = land;
+        this.proprovincie = provincie;
+
+    }
+    // Object maken
+    let veld = new Gegevens(document.forms["registratie"]["exampleInputVoornaam"].value,
+        document.forms["registratie"]["exampleInputNaam"].value,
+        document.forms["registratie"]["exampleInputGebruikersnaam"].value,
+        document.forms["registratie"]["exampleInputAdres"].value,
+        document.getElementById("exampleFormSelectLand").value,
+        document.getElementById("exampleFormControlSelectProvincie").value);
+
+
     // let voorwaarde = document.forms
 
     // Oproepen functies
-    checkEmptyField();
+    errors += checkEmptyField(veld, melding);
     validateWachtwoord();
     checkPC(postcode);
     validateEmail(email);
     // document.forms["registratie"]["exampleFormControlEmail"].value;
     // errors = checkEmptyField(veld, melding);
     meldingBetaling = validatePayment(betaling);
-
-
 
     if (errors == "") {
         document.getElementById("MeldingSucces").style.display = 'block';
@@ -48,24 +63,27 @@ function validateForm() {
 
 // Functcie controleerd velden ofdeze ingevuld zijn
 function checkEmptyField(veld, melding) {
-    // errors verplichte velden
-    // errors += "Het veld voornaam is vereist";
-    // errors += "Het veld naam is vereist";
-    // errors += "Het veld gebruikersnaam is vereist";
-    // errors += "Het veld adres is vereist";
-    // errors += "Het veld land is vereist";
-    // errors += "Het veld provincie is vereist";
-    // validateGebruikersnaam(veld);
+    if (veld.voornaam == "") {
+        melding += "Het veld voornaam is vereist.\n";
+    }
+    if (veld.achternaam == "") {
+        melding += "Het veld naam is vereist.\n";
+    }
+    if (veld.gebruikersnaam == "") {
+        melding += "Het veld gebruikersnaam is vereist.\n";
+    }
+    if (veld.adres == "") {
+        melding += "Het veld adres is vereist.\n";
+    }
+    if (veld.land == "") {
+        melding += "Het veld land is vereist.\n";
+    }
+    if (veld.proprovincie == "") {
+        melding += "Het veld provincie is vereist.\n";
+    }
+    return melding;
 }
 
-// fucntion validateGebruikersnaam(user) {
-//     let first = user.charAt(0);
-//     if (user < 1) {
-//         errors += "Uw gebruikersnaam moet langer zijn dan 1 karakter";
-//     } else if (typeof first != 'string' && typeof first != 'number') {
-//         errors += "Uw gebruikersnaam moet beginnen met een letter of een cijfer";
-//     } else { return true; }
-// }
 
 
 // Functie controle op een geldig email 
@@ -73,11 +91,11 @@ function validateEmail(emailadres) {
 
     let first = emailadres.charAt(0);
     if (first >= 'a' && first >= 'z' || first <= 9 && first >= 0) {
-        errors += "Uw emailadress moet beginnen met een letter of een cijfer";
+        errors += "Uw emailadress moet beginnen met een letter of een cijfer.\n";
     }
     let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
     if (!emailadres.match(pattern)) {
-        errors += "Emailadress is niet correct.";
+        errors += "Emailadress is niet correct.\n";
     }
 }
 
@@ -86,13 +104,13 @@ function validateWachtwoord() {
     let wachtwoord1 = document.forms["registratie"]["inputPassword1"].value;
     let wachtwoord2 = document.forms["registratie"]["inputPassword2"].value;
     if (wachtwoord1 == "") {
-        errors += "Het veld wachtwoord is vereist.";
+        errors += "Het veld wachtwoord is vereist.\n";
     } else if (wachtwoord2 == "") {
-        errors += "Het veld herhaal wachtwoord is vereist.";
+        errors += "Het veld herhaal wachtwoord is vereist.\n";
     } else if (wachtwoord1.length < 7) {
-        errors += "Het wachtwoord moet uit 7 of meer karakters bestaan";
+        errors += "Het wachtwoord moet uit 7 of meer karakters bestaan.\n";
     } else if (wachtwoord1 !== wachtwoord2) {
-        errors += "Je wachtwoorden komen niet overeen";
+        errors += "Je wachtwoorden komen niet overeen.\n";
     }
 }
 
@@ -106,10 +124,10 @@ function validatePayment(veld) {
 // Functie controle postcode
 function checkPC(veld) {
     if (veld == "") {
-        errors += "Het veld postcode is vereist";
+        errors += "Het veld postcode is vereist.\n";
     } // 
     else if (veld < 1000 || veld > 9999) {
-        errors += "De waarde van postcode moet tussen de 1000 en 9999 liggen.";
+        errors += "De waarde van postcode moet tussen de 1000 en 9999 liggen.\n";
     }
 }
 
