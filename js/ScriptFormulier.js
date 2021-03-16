0a648201a0b088a1eb4eb408c4677832f168df70
Elke Oostvogels <r0830699@student.thomasmore.be>
2021-03-16 08:51:13 +0100

    laden, email functie,...

--- js/ScriptFormulier.js
+++ js/ScriptFormulier.js
@@ -1,22 +1,49 @@
 let errors = [];
 let melding = [];
 let meldingBetaling = "";
-let veld = document.forms["registratie"]["exampleInputNaam"];
-veld += document.forms["registratie"]["exampleInputVoornaam"];
+window.onload = VerbergenAlert();
 
-
+function VerbergenAlert() {
+    document.getElementById("MeldingErrors").style.display = 'none';
+    document.getElementById("MeldingSucces").style.display = 'none';
+    document.getElementById("MeldingBetaling").style.display = 'none';
+}
 
 function validateForm() {
+    // waardes toekennen (postcode, betaling, )
+    let betaling = document.getElementById("exampleBetaling").value;
+    let postcode = document.forms["registratie"]["exampleInputPostcode"].value;
+    let email = document.forms["registratie"]["exampleFormControlEmail"].value;
+    // let veld = document.forms["registratie"]["InputNaam"].value;
+    // veld += document.forms["registratie"]["exampleInputVoornaam"].value;
+    // let voorwaarde = document.forms
+
+    // Oproepen functies
     checkEmptyField();
     validateWachtwoord();
-    let postcode = document.forms["registratie"]["exampleInputPostcode"].value;
     checkPC(postcode);
-    document.forms["registratie"]["exampleFormControlEmail"].value;
-    // let mail = validateEmail(emailadres);
-    errors = checkEmptyField(veld, melding);
-    let betaling = document.getElementById("exampleBetaling").value;
+    validateEmail(email);
+    // document.forms["registratie"]["exampleFormControlEmail"].value;
+    // errors = checkEmptyField(veld, melding);
     meldingBetaling = validatePayment(betaling);
-    console.log(meldingBetaling);
+
+
+
+    if (errors == "") {
+        document.getElementById("MeldingSucces").style.display = 'block';
+
+    } else {
+        document.getElementById("MeldingErrors").style.display = 'block';
+        document.getElementById("BoodschapErrors").innerText = errors;
+    }
+
+    if (meldingBetaling != "") {
+        document.getElementById("MeldingBetaling").style.display = 'block';
+        document.getElementById("BoodschapBetaling").innerText = meldingBetaling;
+    }
+
+    // Controle functies
+    console.log(meldingBetaling + errors);
 }
 
 // Functcie controleerd velden ofdeze ingevuld zijn
@@ -28,12 +55,29 @@ function checkEmptyField(veld, melding) {
     // errors += "Het veld adres is vereist";
     // errors += "Het veld land is vereist";
     // errors += "Het veld provincie is vereist";
+    // validateGebruikersnaam(veld);
 }
+
+// fucntion validateGebruikersnaam(user) {
+//     let first = user.charAt(0);
+//     if (user < 1) {
+//         errors += "Uw gebruikersnaam moet langer zijn dan 1 karakter";
+//     } else if (typeof first != 'string' && typeof first != 'number') {
+//         errors += "Uw gebruikersnaam moet beginnen met een letter of een cijfer";
+//     } else { return true; }
+// }
+
 
 // Functie controle op een geldig email 
 function validateEmail(emailadres) {
-    if (emailadres.validity.typeMismatch) {} else {
-        errors += "E-mail adres is niet correct";
+
+    let first = emailadres.charAt(0);
+    if (first >= 'a' && first >= 'z' || first <= 9 && first >= 0) {
+        errors += "Uw emailadress moet beginnen met een letter of een cijfer";
+    }
+    let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    if (!emailadres.match(pattern)) {
+        errors += "Emailadress is niet correct.";
     }
 }
 
@@ -50,7 +94,6 @@ function validateWachtwoord() {
     } else if (wachtwoord1 !== wachtwoord2) {
         errors += "Je wachtwoorden komen niet overeen";
     }
-    console.log(errors, "melding");
 }
 
 // Functie controle betalingswijze
@@ -68,10 +111,7 @@ function checkPC(veld) {
     else if (veld < 1000 || veld > 9999) {
         errors += "De waarde van postcode moet tussen de 1000 en 9999 liggen.";
     }
-    console.log(errors, "melding");
 }
-
-// Functie
 
 
 // Gebruikte websites
@@ -80,8 +120,6 @@ https://www.w3schools.com/howto/howto_js_validation_empty_input.asp
 Controle email-adress
 https://developer.mozilla.org/en-US/docs/Learn/Forms/Form_validation
 Oproepen waarde select option
-https://www.w3schools.com/jsref/prop_select_value.asp*/
-
-
-// errors voorwaarde
-// errors += "Je moet de algemene voorwaarden accepteren.";
+https://www.w3schools.com/jsref/prop_select_value.asp
+Oproepen eerste karakter string
+https://www.w3schools.com/jsref/jsref_startswith.asp */
